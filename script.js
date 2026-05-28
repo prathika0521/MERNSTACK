c6e13a8ae906e82240f91749c4c61b7a39d4b18a
prathika0521 <prathikagatty5@gmail.com>
2026-05-28 15:52:32 +0530

    Added Product list UI project

---
+++ script.js
@@ -0,0 +1,69 @@
+const products = [
+    {
+        name: "Headphones",
+        price: "$50",
+        image: "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd"
+    },
+    {
+        name: "Smart Watch",
+        price: "$80",
+        image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+    },
+    {
+        name: "Laptop",
+        price: "$700",
+        image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+    },
+    {
+        name: "Camera",
+        price: "$400",
+        image: "https://images.unsplash.com/photo-1516035069371-29a1b244cc32"
+    },
+    {
+        name: "short kurthi",
+        price: "$750",
+        image: "https://i.pinimg.com/originals/f3/52/61/f352616a16d406d794ccbfee98ebc42d.jpg"
+    },
+    {
+        name: "earrings",
+        price:"$200",
+        image: "https://tse1.mm.bing.net/th/id/OIP.AiulUYKp4R27Bqhw-pN9HAHaHa?pid=Api&P=0&h=180"
+    },
+    {
+        name: "pen",
+        price: "$10",
+        image: "https://tse4.mm.bing.net/th/id/OIP.7lwhlToRm2n3UrGYBg-QEAHaE6?pid=Api&P=0&h=180"
+    }
+];
+
+const container = document.getElementById("productContainer");
+
+function displayProducts(items) {
+    container.innerHTML = "";
+
+    items.forEach(product => {
+        container.innerHTML += `
+            <div class="card">
+                <img src="${product.image}" alt="${product.name}">
+                <h3>${product.name}</h3>
+                <p class="price">${product.price}</p>
+                <button>Add to Cart</button>
+            </div>
+        `;
+    });
+}
+
+displayProducts(products);
+
+function filterProducts() {
+    const search = document
+        .getElementById("search")
+        .value
+        .toLowerCase();
+
+    const filtered = products.filter(product =>
+        product.name.toLowerCase().includes(search)
+    );
+
+    displayProducts(filtered);
+}
